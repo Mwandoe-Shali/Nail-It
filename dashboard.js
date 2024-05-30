a2303cd7e8bb015f1174bed5d24040837b6b0664
Mwandoe-Shali <mwandoeshali@gmail.com>
2024-05-30 07:21:10 +0300

    feat: Add Firebase SDK for authentication, Firestore, and storage. Node js is unresponsive at the moment, don't know the error.

--- dashboard.js
+++ dashboard.js
@@ -1,3 +1,10 @@
-document.getElementById('addSiteButton').addEventListener('click', function() {
-    document.getElementById('addSiteButton').innerHTML = 'Button clicked!';
-});
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-app.js"
+import { getDatabase } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-database.js"
+
+const appSettings = {
+    databaseURL: "https://nail-it-8f766-default-rtdb.asia-southeast1.firebasedatabase.app/"
+}
+const app = initializeApp(appSettings)
+
+console.log(app)
+
